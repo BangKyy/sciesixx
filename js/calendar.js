84c9b06dd9dd8a0d95f03f995754bb0f69071458
mby444 <bimayudha161@gmail.com>
2023-02-02 20:11:19 +0700

    Add calendar html structure

---
+++ js/calendar.js
@@ -0,0 +1,20 @@
+import * as nav from "./partials/navbar.js";
+import * as footer from "./partials/footer.js";
+import { generateDynamicSiteName } from "./utils/site-name.js";
+import { UpArrow } from "./partials/float.js";
+
+window.addEventListener("scroll", () => {
+    nav.toggleNavShadow(document, window.scrollY);
+});
+
+window.addEventListener("load", () => {
+    generateDynamicSiteName("../../json/config.json");
+    nav.initSidebar();
+    nav.initSidebarArrow();
+    nav.toggleSignBtn(document, null, "../../logout");
+    nav.toggleSignBtn(document, ".sign-button-list", "../../logout");
+    nav.checkUsername();
+    footer.generateDynamicCopyrightYear(document);
+    footer.updateSocmedUrl(document, "../../json/social-media.json");
+    new UpArrow(".up-arrow-container").init();
+});
